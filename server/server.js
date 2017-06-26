8953c6ba3a5e8155f44bbb2e662aa767911eef6f
Melanie Burger <melanie.burger@rangle.io>
2017-06-25 21:18:54 -0400

    Setup a basic CRUD API

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-var _ = require('lodash');
+var R = require('ramda');
 
 // express.static will serve everything
 // with in client as a static resource
@@ -18,10 +18,48 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 
-var lions = [];
-var id = 0;
+let lions = []
+let id = 0;
 
-// TODO: make the REST routes to perform CRUD on lions
+app.route('/lions')
+  .get((req, res) => {
+    res.json(lions);
+  })
+  .post((req, res) => {
+    const lion = Object.assign({}, req.body, { id: R.toString(id) })
+    lions = [ ...lions, lion ]
+    id += 1;
+    res.json(lion)
+  })
+
+app.route('/lions/:id')
+  .get((req, res) => {
+    const { id } = req.params
+    res.json(R.find(R.propEq('id', id), lions));
+  })
+  .put((req, res) => {
+    const { id } = req.params
+    const lionToUpdateIndex = R.findIndex(R.propEq('id', id), lions);
+    
+    if (lionToUpdateIndex < 0) {
+      res.send()
+    } else {
+      const lionToUpdate = R.find(R.propEq('id', id), lions)
+      const updatedLion = R.merge(lionToUpdate, req.body)
+      lions = [
+        ...lions.slice(0, lionToUpdateIndex),
+        ...lions.slice(lionToUpdateIndex + 1),
+        updatedLion
+      ]
+      res.json(updatedLion);
+    }
+  })
+  .delete((req, res) => {
+    const { id } = req.params
+    const lionToDelete = R.find(R.propEq('id', id), lions)
+    lions = lions.filter( lion => R.toString(lion.id) !== R.toString(id) );
+    res.json(lionToDelete)
+  });
 
 app.listen(3000);
 console.log('on port 3000');
